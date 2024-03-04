76b05fbbbfdc0089c5dacca188b7a7882aa03899
jalmx <jalmx@users.noreply.github.com>
2024-03-04 20:46:04 +0000

    deploy: af21d9533842165f3ba114bcf976388627afd1b2

--- codigos/mat.c
+++ codigos/mat.c
@@ -1,23 +1,21 @@
 #include <math.h>
 #include <stdio.h>
 
-int main()
-{
-    int potencia = pow(2,3);
-    int raiz = sqrt(25);
-    int redondeoArriba = floor(25.3);
-    int redondeoAbajo = ceil(25.7);
-    float coseno = cos(45);
-    float seno = sin(45);
-    float tangente = tan(45);
+int main(){
+    int valor1 = 4;
+    int valor2 = 3;
 
-    
-    printf("2 al cubo es: %d \n", potencia);
-    printf("la raiz de 25 es: %d \n", raiz);
-    printf("redondeo arriaba de 25.3 es: %d \n", redondeoArriba);
-    printf("redondeo abajo de 25.7 es: %d \n", redondeoAbajo);
-    printf("coseno de 45 es: %.2f \n", coseno);
-    printf("seno de 45 es: %.2f \n", seno);
-    printf("tangente de 45 es: %.2f \n", tangente);
+    int suma = valor1 + valor2;
+    int resta = valor1 - valor2;
+    int multiplicacion = valor1 * valor2;
+    float division = valor1 / (float) valor2;
+    int modulo = valor1 % valor2;
+
+    printf("La suma es %i \n", suma);
+    printf("La resta es %i \n", resta);
+    printf("La multiplicación es %d \n", multiplicacion);
+    printf("La división es %f \n", division);
+    printf("El residuo es %i \n", modulo);
+
     return 0;
 }

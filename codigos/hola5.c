c754bac2799c6c4871b36bdd09ff61468a37534c
jalmx <jalmx@users.noreply.github.com>
2024-04-15 20:32:48 +0000

    deploy: a758337008a33bf8ef882c580522fef1c6f6622a

---
+++ codigos/hola5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int contador = 0;
+
+    while (contador <= 5){
+        printf("%d\n", contador);
+        contador = contador + 1;     
+    }
+    
+    return 0;
+}
